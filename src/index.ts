5e0530ea882b11966647b90b4de9291979cd9360
Maxim Yanovskiy <myanovskiy@evolutiongaming.com>
2018-02-12 19:00:29 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 process.stdin.resume();
 process.stdin.setEncoding('ascii');
 
-var input_stdin = "";
-var input_stdin_array: string[];
-var input_currentline = 0;
 console.log = () => undefined;
 declare const SOLUTION_PATH: string;
 
-process.stdin.on("data", function (data: any) {
-    input_stdin += data;
+let stdinInput = "";
+process.stdin.on("data", function (data: string) {
+    stdinInput += data;
 });
 
 process.stdin.on("end", function () {
-    input_stdin_array = input_stdin.trim().split("\n");
+    let input = stdinInput.trim().split("\n");
     const solution = require(SOLUTION_PATH);
-    const result = solution.default(input_stdin_array).toString();
+    const result = solution.default(input).toString();
     if (result) {
         process.stdout.write(result + "\n");
     }
---
+++ src/week2/problem2/problem2.spec.ts
@@ -0,0 +1,4 @@
+export default function(input: string[]) {
+    const [restrictions, dishes] = input[0].split(" ").map(Number);
+    
+}
